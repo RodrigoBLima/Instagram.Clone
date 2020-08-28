f5b2e68aed8f012abd8eb53968dc421a187e827a
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-08-27 21:52:17 -0300

    configure macro of the project

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import Feed from './pages/Feed'
+import Feed from './pages/Feed';
 
-const Routes = createAppContainer(
-    createSwitchNavigator({
-        Feed
-    })
-)
-export default Routes;
+import logo from './assets/instagram.png';
+
+export default createAppContainer(
+  createStackNavigator({
+    Feed,
+  }, {
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerTitle: <Image source={logo} />,
+      headerStyle: {
+        backgroundColor: '#f5f5f5'
+      },
+    },
+  }),
+);
