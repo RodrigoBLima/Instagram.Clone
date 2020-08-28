9e217f0f71c2406f478330867b495b2e49296fc5
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-08-27 21:21:55 -0300

    styles, estructure components

---
+++ src/components/LazyImage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const LazyImage: React.FC = () => {
+  return (
+    <Container>
+      <Text>LazyImage</Text>
+    </Container>
+  );
+};
+
+export default LazyImage;
---
+++ src/pages/Feed/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+    <Container>
+      <Text>Feed</Text>
+    </Container>
+  );
+};
+
+export default Feed;
